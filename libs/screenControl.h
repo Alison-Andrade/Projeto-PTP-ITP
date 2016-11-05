52f2c44faac19a0edb57e14983f2b3a1fd78841d
AlisonSidnei <sidneialison@gmail.com>
2016-11-04 22:41:53 -0200

    Add files via upload

--- libs/screenControl.h
+++ libs/screenControl.h
@@ -3,7 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-	
+
+//Funções telaInicial.c	
 void telaInicial();
+
+//Funções telaFuncionário.c
+void telaFuncionario();
+
+//Funções telaCliente.c
 	
 #endif
--- telaInicial.c
+++ telaInicial.c
@@ -1,22 +1,25 @@
 #include "libs/screenControl.h"
 
+
 void telaInicial(){
 	system("clear");
+
+	int opcao;
 	
 	//Tela de apresentação inicial, e seleção de cliente ou funcionário
 	printf("----------------------------------------------------------------------------------------------\n");
 	printf("\n********************************** Seja bem vindo! *******************************************\n\n");
 	printf("----------------------------------------------------------------------------------------------\n");
-	printf("				Você é: 	(por favor digite o numero da opção desejada)\n\n\n");
-	printf("		1 - Funcionário		2 - Cliente\n\n");
+	printf("\n\n					Você é: 	\n\n\n");
+	printf("			1 - Funcionário		2 - Cliente\n\n");
+	printf("\n\n		(Por favor digite o numero da opção desejada.)\n");
 	
 	printf("\n\n\n\n");
-	printf("----------------------------------------------------------------------------------------------\n");
-	printf("					Desenvolvido por: Alison Sidnei () / Pedro Victor ()\n");
-	printf("----------------------------------------------------------------------------------------------\n");	
 
-	/*switch(){
+	scanf("%d", &opcao);
+	
+	switch(opcao){
 
-		case 1: ; 
-	}*/
+		case 1: telaFuncionario(); 
+	}
 }
